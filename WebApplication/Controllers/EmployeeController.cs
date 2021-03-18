c0af3ce48731d87df9556bbbc1e0e3a8c8886530
ilkerogutcu <ilkerogtc@gmail.com>
2021-03-18 23:32:57 +0300

    EmployeeController modified

--- WebApplication/Controllers/EmployeeController.cs
+++ WebApplication/Controllers/EmployeeController.cs
@@ -30,45 +30,5 @@ namespace WebApplication.Controllers
         {
             return _employeeService.GetAll();
         }
-        
-        [HttpGet]
-        [Route("Files")]
-        public ActionResult Gets()
-        {
-            string[] filePaths = Directory.GetFiles("./logs/");
-            var files = new List<FileModel>();
-            foreach (string filePath in filePaths)
-            {
-                files.Add(new FileModel()
-                {
-                    FileName = Path.GetFileName(filePath),
-                    FilePath = filePath
-                });
-            }
-
-            return null;
-        }
-        
-        [HttpPost]
-        [Route("Download")]
-        public ActionResult Index(DateTime startDate,DateTime endDate)
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            using var iterator = Utilities.GetDateRange(startDate, endDate).GetEnumerator();
-            using var zip = new ZipFile {AlternateEncodingUsage = ZipOption.AsNecessary};
-            zip.AddDirectoryByName("Files");
-
-            while (iterator.MoveNext())
-            {
-                var logDate = iterator.Current.ToString("yyyy.MM.dd").Replace(".","");
-                zip.AddFile($"./logs/{logDate}.txt", "Files");
-            }
-          
-            var zipName = $"Zip_{DateTime.Now:yyyy-MMM-dd-HHmmss}.zip";
-            using var memoryStream = new MemoryStream();
-            zip.Save(memoryStream);
-         
-            return File(memoryStream.ToArray(), "application/zip", zipName);
-        }
     }
 }
